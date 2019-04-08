6fca057bbb60bdd6d82f66087076df392a4dab61
Matan Peled <mip@cs.technion.ac.il>
2019-04-08 13:57:08 +0300

    Move to own package.

--- src/main/kotlin/il/ac/technion/cs/softwaredesign/storage/SecureStorage.kt
+++ src/main/kotlin/il/ac/technion/cs/softwaredesign/storage/SecureStorage.kt
@@ -1,4 +1,4 @@
-package il.ac.technion.cs.softwaredesign
+package il.ac.technion.cs.softwaredesign.storage
 
 /**
  * Write to a key. If this key was already associated to some value, the previous value is discarded.
@@ -9,9 +9,9 @@ package il.ac.technion.cs.softwaredesign
 fun write(key: ByteArray, value: ByteArray) = Unit
 
 /**
- * Read value from a key. If there is no value for this key, an empty byte array is returned.
+ * Read value from a key. If there is no value for this key, null is returned.
  *
  * @param key Data to be used as a key.
- * @return Value associated with [key].
+ * @return Value associated with [key], or null.
  */
-fun read(key: ByteArray): ByteArray = byteArrayOf()
+fun read(key: ByteArray): ByteArray? = byteArrayOf()
