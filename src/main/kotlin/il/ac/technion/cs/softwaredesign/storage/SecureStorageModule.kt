06e03cbab6c1b406952755c39b7a52cb396ed12e
Matan Peled <mip@cs.technion.ac.il>
2019-05-05 12:53:11 +0300

    Actually don't bind SecureStorage.

--- src/main/kotlin/il/ac/technion/cs/softwaredesign/storage/SecureStorageModule.kt
+++ src/main/kotlin/il/ac/technion/cs/softwaredesign/storage/SecureStorageModule.kt
@@ -2,11 +2,9 @@ package il.ac.technion.cs.softwaredesign.storage
 
 import com.authzee.kotlinguice4.KotlinModule
 import il.ac.technion.cs.softwaredesign.storage.impl.SecureStorageFactoryImpl
-import il.ac.technion.cs.softwaredesign.storage.impl.SecureStorageImpl
 
 class SecureStorageModule : KotlinModule() {
     override fun configure() {
-        bind<SecureStorage>().to<SecureStorageImpl>()
         bind<SecureStorageFactory>().to<SecureStorageFactoryImpl>()
     }
 }
